45e6e197000e73be1638d26122496ee89b9a9074
Serch-ING <78749398+Serch-ING@users.noreply.github.com>
2021-08-18 00:43:04 -0600

    Detalles

--- main.py
+++ main.py
@@ -1,28 +1,29 @@
 from cargar import *
 from reportes import *
 
-#Menu desplegado
-def menu():
-    print('1.Cargar archivo\n2.Mostrar reporte en consola\n3.Exportar reporte\n4.Salir')
-print("\n----------Bienvenido----------\n")
-menu()
-
-option = int(input("Ingrese una opcion: "))
-
-while option !=0:
-    if option == 1:
-        purificacionExtra()
-        #purificacion()
-    elif option == 2:
-        MostrarReportsConsole()
-    elif option == 3:
-        GenerarReportes()
-    elif option == 4:
-        print("\nGracias por usar!")
-        exit()
-    else:
-        print("Opcion invalida.")
-
-    print()
+if __name__ == "__main__":
+    #Menu desplegado
+    def menu():
+        print('1.Cargar archivo\n2.Mostrar reporte en consola\n3.Exportar reporte\n4.Salir')
+    print("\n----------Bienvenido----------\n")
     menu()
+
     option = int(input("Ingrese una opcion: "))
+
+    while option !=0:
+        if option == 1:
+            purificacionExtra()
+            #purificacion()
+        elif option == 2:
+            MostrarReportsConsole()
+        elif option == 3:
+            GenerarReportes()
+        elif option == 4:
+            print("\nGracias por usar!")
+            exit()
+        else:
+            print("Opcion invalida.")
+
+        print()
+        menu()
+        option = int(input("Ingrese una opcion: "))
